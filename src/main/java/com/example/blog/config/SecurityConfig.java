ba14d3ad1070c2fc975bd264d0bdc8ba29fabe75
Kumar Himanshu (CSS ICW ENG SW BTFW SVT) <Himanshu.Kumar@infineon.com>
2024-04-24 22:54:57 +0530

    added application properties to work with different environment

--- src/main/java/com/example/blog/config/SecurityConfig.java
+++ src/main/java/com/example/blog/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.example.SpringSecurityJWT.config.JwtAuthFilter;
 import com.example.blog.Security.JwtAuthenicationEntryPoint;
 import com.example.blog.Security.JwtAuthenticationFilter;
 
--- src/main/java/com/example/blog/controllers/AuthController.java
+++ src/main/java/com/example/blog/controllers/AuthController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.SpringSecurityJWT.User.auth.AuthenticationResponse;
 import com.example.blog.Security.JwtHelper;
 import com.example.blog.entities.JwtRequest;
 import com.example.blog.entities.JwtResponse;
